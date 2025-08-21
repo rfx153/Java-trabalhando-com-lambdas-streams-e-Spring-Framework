c134ebfc03681fc300198498a2bcb9bb96ae353a
Rafael Cardoso <rafaelfx153@gmail.com>
2025-08-21 13:00:02 -0300

    fim da aula 02 parte 11

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoApi;
@@ -31,12 +32,23 @@ public class Principal {
 
         List<DadosTemporada> temporadas = new ArrayList<>();
 
-		for(int i = 1; i<=dados.totalTemporadas(); i++){
+		/*for(int i = 1; i<=dados.totalTemporadas(); i++){
         json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season="+ i +API_KEY);
 		DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
 		temporadas.add(dadosTemporada);
 		}
-		temporadas.forEach(System.out::println);
+		temporadas.forEach(System.out::println);*/
+    /* 
+            for(int i =0; i< dados.totalTemporadas(); i++){
+                List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+                for (int j =0; j < episodiosTemporada.size(); j++){
+                    System.out.println(episodiosTemporada.get(j).titulo());
+
+                }
+            }
+    */
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+        temporadas.forEach(System.out::println );
 
     }
 }

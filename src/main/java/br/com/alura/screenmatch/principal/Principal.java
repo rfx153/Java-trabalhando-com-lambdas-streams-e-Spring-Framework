80aad43d21fed204b013578641612ba3d89bf6d3
Rafael Cardoso <rafaelfx153@gmail.com>
2025-08-25 11:55:03 -0300

    finalizei a aula 04 parte 05

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -77,24 +78,37 @@ public class Principal {
 
             /*dadosEpisodios.add(new DadosEpisodio("teste", 3, "10" ,"2020-01-01" ));
             dadosEpisodios.forEach(System.out::println);*/
-            System.out.println("\n Top 5 episódios");
-            dadosEpisodios.stream()
-                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
-                .peek(e -> System.out.println("primeiro filtro(N/A)" + e))
-                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
-                .peek(e -> System.out.println("ordenação " + e))
-                .limit(10)
-                .peek(e -> System.out.println("Limite " + e))
-                .map(e -> e.titulo().toUpperCase())
-                .peek(e -> System.out.println("Mapeamento " + e))
-                .forEach(System.out::println);
+            // System.out.println("\n Top 5 episódios");
+            // dadosEpisodios.stream()
+            //     .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+            //     .peek(e -> System.out.println("primeiro filtro(N/A)" + e))
+            //     .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+            //     .peek(e -> System.out.println("ordenação " + e))
+            //     .limit(10)
+            //     .peek(e -> System.out.println("Limite " + e))
+            //     .map(e -> e.titulo().toUpperCase())
+            //     .peek(e -> System.out.println("Mapeamento " + e))
+            //     .forEach(System.out::println);
 
-            // List<Episodio> episodios = temporadas.stream()
-            // .flatMap(t-> t.episodios().stream()
-            // .map(d-> new Episodio(t.numero(), d)))
-            // .collect(Collectors.toList());
+            List<Episodio> episodios = temporadas.stream()
+            .flatMap(t-> t.episodios().stream()
+            .map(d-> new Episodio(t.numero(), d)))
+            .collect(Collectors.toList());
 
-            // episodios.forEach(System.out::println);
+             episodios.forEach(System.out::println);
+
+            System.out.println("Digite o texto do titulo do ep") ;
+            var trechoTitulo = leitura.nextLine();
+            Optional<Episodio> episodioBuscado = episodios.stream()
+                .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
+                .findFirst();
+            if (episodioBuscado.isPresent()){
+                System.out.println("episódio encontrando");
+                System.out.println("temporada: " + episodioBuscado.get().getTemporada());
+            }
+            else {
+                System.out.println("episódio não encontrado");
+            }
 
             // System.out.println("A partir de que ano você deseja ver os eps?");
             // var ano = leitura.nextInt();

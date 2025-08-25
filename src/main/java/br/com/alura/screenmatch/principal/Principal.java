79b2b3115d04c92e41eb15770952e7f611805578
Rafael Cardoso <rafaelfx153@gmail.com>
2025-08-25 13:37:14 -0300

    finalizei a aula 04 parte 10

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -6,6 +6,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -136,6 +137,13 @@ public class Principal {
                  Collectors.averagingDouble(Episodio::getAvaliacao)));
         System.out.println(avaliacoesPorTemporada);
 
+        DoubleSummaryStatistics est = episodios.stream()
+            .filter(e -> e.getAvaliacao() > 0.0 )
+            .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+            System.out.println("Média: " + est.getAverage());
+            System.out.println("Melhor episódio: " + est.getMax());
+            System.out.println("Pior episódio: " + est.getMin());
+            System.out.println("Quantidade: " + est.getCount());
     }
 }
 
